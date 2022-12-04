d02afb5226a09bc78b8e5f813ef4bbd6e02b1ef5
Max Phillips <max@maxtop.local>
2022-12-04 15:18:06 +1100

    setup terraform for ecs

---
+++ hasura/variables.tf
@@ -0,0 +1,19 @@
+variable "app_image" {
+  type = string
+}
+
+variable "app_port" {
+  type = number
+}
+
+variable "fargate_cpu" {
+  type = number
+}
+
+variable "fargate_memory" {
+  type = number
+}
+
+variable "aws_region" {
+  type = string
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ecr_repository_name" {
   description = "The name of the ECR repository"
-  type = string
-  default = "my-ecr-repository"
+  type        = string
+  default     = "my-ecr-repository"
 }
